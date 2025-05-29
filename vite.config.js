701f3ea7944a3881566679e0d3a673949b45002c
ghostnarassin <muksalmina.nuruddin@gmail.com>
2025-05-29 19:07:00 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
     root: '.',
+    base: '/procon',
     build: {
-        outDir: 'dist'
+        outDir: 'dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: 'src/index.js'
+        }
     },    
     server: {
         port: 8000,
   }
 
-}
+})

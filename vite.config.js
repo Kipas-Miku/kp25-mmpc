49a9975d26258636ca0f63d28f6e808691384b4a
Narassin <37112149+Narassin@users.noreply.github.com>
2025-06-14 14:02:09 +0800

    Deploy Test Commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: '.',
-    base: '/procon',
+    base: '/kp25-mmpc/',
     build: {
         outDir: 'dist',
         emptyOutDir: true,

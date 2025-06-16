a9e144aacbb5ebed5226454eae0d2251e66bea32
ghostnarassin <muksalmina.nuruddin@gmail.com>
2025-06-16 18:09:27 +0800

    Updates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 // Libraries
 import { Player,stringToDataUrl } from "textalive-app-api";
 import { songs } from "../songs";
+import { LyricsPlayer } from "./app.js";
 import Parallax from "parallax-js";
 
 
 
-var scene = document.getElementById('mainMenu');
+var scene = document.getElementById('paraBg');
 var parallaxInstance = new Parallax(scene,{
     relativeInput: true,
     invertX: true,
     invertY: true,
-    hoverOnly: true,
 });
 
 // Variable
@@ -30,10 +30,11 @@ class Main
             }
         });
 
+        this.lyrPlayer = new LyricsPlayer(this._player, this.router);
+
         this.mainMenu();
         this.setupCredit();
         this.setupSongSelector();
-        this.setupLyricApp();
     }
 
     mainMenu() {
@@ -54,15 +55,14 @@ class Main
             const li = document.createElement("li");
             li.className = "list-group-item list-group-item-action text-dark";
             li.textContent = `${song.title} / ${song.artist}`;
-            li.onclick = () => this.playSong(song);
+            li.onclick = () => this.lyrPlayer.loadAndPlay(song);
+            // li.onclick = () => this.playSong(song);
+            
             ul.appendChild(li);
         });
         document.getElementById("ssReturnMenu").onclick = () => this.router.navigate("mainMenu");
     }
 
-    setupLyricApp(){
-        document.getElementById("lsReturnMenu").onclick = () => this.router.navigate("mainMenu");
-    }
 
     playSong(song) {
         this.router.navigate("player-ui");
@@ -76,21 +76,41 @@ class Main
     }
 
     _onAppReady(){
-
+        if (!app.songUrl)
+        {
+            // インフォーマルダイブ / 99piano
+            this._player.createFromSongUrl("https://piapro.jp/t/Ppc9/20241224135843", {
+                video: {
+                // 音楽地図訂正履歴
+                beatId: 4694277,
+                chordId: 2830732,
+                repetitiveSegmentId: 2946480,
+            
+                // 歌詞URL: https://piapro.jp/t/77V2
+                // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FPpc9%2F20241224135843
+                lyricId: 67812,
+                lyricDiffId: 20656
+                },
+            });
+        }
+        document.getElementById("playBtn").addEventListener("click", () => {
+            this._player.requestPlay();
+        })
     }
 
     _onVideoReady (v)
     {
-
         document.getElementById("loading").style.display = "none";
         const song = this._player.data.song;
         metaEl.innerHTML = `<h2>${song.name}</h2><p>${song.artist.name}</p>`;
-    
+      
     }
 
     _onTimeUpdate(){
 
     }
+
+
 }
 
 class App {

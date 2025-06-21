8fa4184ec4d4f9cf5a5832201f51837b724652f9
Narassin <37112149+Narassin@users.noreply.github.com>
2025-06-22 02:26:58 +0800

    Revamping the index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import { Router } from "./router.js";
+import { UIManager } from "./Controller/uiManager";
+import { MenuUI  } from "./View/menuUI";
+import { Router } from "./Controller/routerManager";
 import Parallax from "parallax-js";
 
 
 class Main
 {
     constructor() {
-        this._router = new Router();
-
+        // this._router = new Router();
+        const uiManager = new UIManager();
+        uiManager.show(MenuUI, {
+        onSelectSong: () => {
+            console.log("Go to Song List");
+            // uiManager.show(SongSelectorUI);
+        },
+        onCredit: () => {
+            console.log("Go to Credit");
+            // uiManager.show(CreditUI);
+        },
+        onExit: () => {
+            console.log("Exiting Game");
+        },
+        onSetting: () => {
+            console.log("Go to Settings");
+        }
+        });
         var scene = document.getElementById('paraBg');
         var parallaxInstance = new Parallax(scene,{
             relativeInput: true,
